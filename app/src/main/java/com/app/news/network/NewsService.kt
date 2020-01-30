597f0f877e27b70d3fb49b1035d18dcb5a4a2f31
sri.san29@gmail.com <GitHub@29>
2020-01-30 17:08:29 +0530

    Code Cleanup

--- app/src/main/java/com/app/news/network/NewsService.kt
+++ app/src/main/java/com/app/news/network/NewsService.kt
@@ -4,7 +4,6 @@ import com.app.news.feature.topnews.model.TopNews
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -24,9 +23,6 @@ interface NewsService {
             return retrofit.create(NewsService::class.java)
         }
     }
-
-    @GET("/v2/top-headlines?country=in&apiKey=1a38df8182df448cbea6721f8e9638d9")
-    suspend fun getTopNews(): Response<TopNews>
 
     @GET("/v2/top-headlines?country=in&apiKey=1a38df8182df448cbea6721f8e9638d9")
     suspend fun getHeadlines(): TopNews?
